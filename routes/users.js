e447c761499dae2fda7f29e1136c27cc65c0242a
ryanrrogers <ryan.rogers.developer@gmail.com>
2021-04-14 12:50:13 -0400

    continued work on signup page, added some functionality

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
-
 const express = require('express')
 const router = express.Router()
-
+const User = require('../models/user.js')
 
 router.get('/login',(req,res)=>{
     res.render('login')
@@ -11,7 +10,46 @@ router.get('/signup',(req,res)=>{
     })
 
 router.post('/signup',(req,res)=>{
+	const {name, email, password, password_2} = req.body
+	let errors = []
+	console.log('Username: ' + name + ' email: ' + email + ' password: ' + password)
+	if(!name || !email || !password || !password_2) {
+		errors.push({msg: 'Please fill out required fields'})
+	}
+
+	if(password !== password_2) {
+		errors.push({msg: 'Passwords do not match!'})
+	}
+
+	if(password.length < 8) {
+		errors.push({msg: 'Password must be 8 characters long!'})
+	}
+
+	if(errors.length > 0) {
+		res.render('signup', {
+			errors: errors,
+			name: name,
+			email: email,
+			password: password,
+			password_2: password_2
+		})
+	} else {
+		User.findOne({email: email}).exec((err, user) => {
+			console.log(user)
+			if(user) {
+				errors.push({msg: 'email already in use!'})
+				render(res, errors, name, email, password, password_2)
+			} else {
+				const newUser = new User({
+					name: name,
+					email: email,
+					password: password
+				})
+			}
+		})
+	}
 })
+
 router.post('/login',(req,res,next)=>{
   })
 
